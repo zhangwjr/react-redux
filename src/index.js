b8b2cf7f40119162d96f4411203ed74f92f62d05
zhangwjr <zhangwjr@yonyou.com>
2019-08-22 19:51:20 +0800

    fix name

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { HashRouter, Route } from 'react-router-dom';
 import Counter from './components/Counter'
-import DemoApp from './components/Counter_Redux'
+import Counter_Redux from './components/Counter_Redux'
 
 let Home = () => <div>Home</div>;
 let Profile = () => <div>Profile</div>;
@@ -11,7 +11,7 @@ let User = () => <div>User</div>;
 ReactDOM.render(<HashRouter>
     <div>
         <Route path='/' component={Counter} />
-        <Route path='/demo' component={DemoApp} />
+        <Route path='/demo' component={Counter_Redux} />
         <Route path='/home' component={Home} />
         <Route path='/profile' component={Profile} />
         <Route path='/user' component={User} />
